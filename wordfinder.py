8906d748fc5a7d4e495fc38206d8edf0aa683482
pixls <tha.pixls@gmail.com>
2022-01-25 17:09:57 -0500

    finished exercise

--- wordfinder.py
+++ wordfinder.py
@@ -1,12 +1,13 @@
 """Word Finder: finds random words from a dictionary."""
 
+from random import randint
 
 class WordFinder:
     """
     reads a list of words from a given file and can return a random word from that list
     
     >>> wf = WordFinder("words.txt")
-    3 words read
+    235887 words read
 
     >>> wf.random()
     'cat'
@@ -22,11 +23,24 @@ class WordFinder:
     
     """
     def __init__(self, path):
+        """records the file to a list,"""
         self.file = open(path)
-        self.line_count = self.lines()
+        self.line_list = self.file.read().split('\n')
+        self.line_count = len(self.line_list)
+        print(f"{self.line_count} words read")
+        self.file.close()
     
-    def lines(self):
-        lines = 0
-        for line in self.file:
-            lines += 1
-        return lines
+    def random(self):
+        """returns a random line from the line list"""
+        line_num = randint(0, self.line_count)
+        return self.line_list[line_num]
+
+class SpecialWordFinder(WordFinder):
+    """
+    expands WordFinder to be able to parse files with comments and blanks
+    """
+    def __init__(self, path):
+        super().__init__(path)
+        self.line_list = [item for item in self.line_list if item and item[0] != "#"]
+        self.line_count = len(self.line_list)
+        print(f"but actually {self.line_count} words read")
